240fe04d3958d356b0019c566fe3e19a5c624958
Alex Stuckey <alex@stuckey.org.uk>
2018-07-02 14:28:50 +0100

    Setup 404 and wrong url handling

--- app/application/config/routes.php
+++ app/application/config/routes.php
@@ -49,9 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
-$route['404_override'] = '';
+$route['default_controller'] = 'home/homepage';
+$route['404_override'] = 'home/show404';
 $route['translate_uri_dashes'] = FALSE;
 
 
 $route['home'] = 'home/homepage';
+
+
+
+$route['(.+)'] = 'home/show404';
