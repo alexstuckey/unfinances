15815fc5b0d29a3b1b05d72c4791b47076cf0cfc
Alex Stuckey <alex@stuckey.org.uk>
2018-07-02 16:29:56 +0100

    Set up a placeholder template

--- app/application/controllers/Home.php
+++ app/application/controllers/Home.php
@@ -8,4 +8,24 @@ class Home extends CI_Controller {
         show_404();
     }
 
+    public function homepage()
+    {
+        // $this->load->model('User_model');
+        // if (!$this->User_model->doesUserExist($_SERVER['REMOTE_USER'])) {
+        //     $this->load->helper('url');
+        //     redirect('/onboard/welcome');
+        // } else if ($this->User_model->isAdmin($_SERVER['REMOTE_USER'])) {
+        //     $data['is_admin'] = TRUE;
+        // }
+
+        $data['active'] = 'home';
+        $data['page_title'] = 'UCFinances - Home';
+        
+        $this->load->view('header', $data);
+
+        $this->load->view('homepage', $data);
+
+        $this->load->view('footer', $data);
+    }
+
 }
---
+++ app/application/views/homepage.php
@@ -0,0 +1,9 @@
+<!-- Content Block (Contains Sidebars and central columns) -->
+<main class="container-fluid" id="content">
+    <div class="jumbotron">
+        <h1>Welcome to UC Finances</h1>
+        <p class="lead">
+            Currently unfunctional.
+        </p>
+    </div>
+</main>
