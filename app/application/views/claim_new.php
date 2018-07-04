b8721f86a081226031030284e9fc2bbcc4796bc8
Alex Stuckey <alex@stuckey.org.uk>
2018-07-04 19:04:57 +0100

    Shifting data load to JS

--- app/application/views/claim_new.php
+++ app/application/views/claim_new.php
@@ -10,37 +10,35 @@
             <h4 class="mb-3">Claim details</h4>
             <form>
                 <div class="mb-3">
-                    <label class="bold-label" for="username">Claim №</label>
-                    <input type="text" class="form-control" id="firstName" placeholder="" value="17" required="" style="background-color: transparent; border: 0; color: #212529;">
+                    <label class="bold-label" for="input_id_claim">Claim №</label>
+                    <input type="text" class="form-control" id="input_id_claim" value="" required="" style="background-color: transparent; border: 0; color: #212529;">
                 </div>
                 <div class="row">
                     <div class="col-md-6 mb-3">
-                        <label class="bold-label" for="firstName">Claimant Name</label>
-                        <input type="text" class="form-control" id="firstName" placeholder="" value="" required="" style="background-color: transparent; border: 0; color: #212529;">
+                        <label class="bold-label" for="input_claimant_name">Claimant Name</label>
+                        <input type="text" class="form-control" id="input_claimant_name" value="" required="" style="background-color: transparent; border: 0; color: #212529;">
                     </div>
                     <div class="col-md-6 mb-3">
-                        <label class="bold-label" for="lastName">CIS Username</label>
-                        <input type="text" class="form-control" id="lastName" placeholder="" value="" required="" style="background-color: transparent; border: 0; color: #212529;">
+                        <label class="bold-label" for="input_claimant_id">CIS Username</label>
+                        <input type="text" class="form-control" id="input_claimant_id" value="" required="" style="background-color: transparent; border: 0; color: #212529;">
                     </div>
                 </div>
 
                 <div class="mb-3">
-                    <label class="bold-label" for="username">Date</label>
+                    <label class="bold-label" for="input_date">Date</label>
                     <div class="input-group">
-                        <input type="text" class="form-control" id="username" placeholder="Username" required="">
+                        <input type="text" class="form-control" id="input_date" placeholder="Date" required="" value="<?php echo $claim['date'] ?>">
                     </div>
                 </div>
 
                 <div class="mb-3">
-                    <label class="bold-label" for="email">Cost Centre</label>
-                    <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                    <label class="bold-label" for="input_of_id_cost_centre">Cost Centre</label>
+                    <input type="text" class="form-control" id="input_of_id_cost_centre" placeholder="Cost Centre" value="">
                 </div>
 
                 <div class="mb-3">
-                    <label class="bold-label" for="username">Description of expense</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="username" placeholder="Username" required="">
-                    </div>
+                    <label class="bold-label" for="input_description">Description of expense</label>
+                    <input type="text" class="form-control" id="input_description" placeholder="Description" value="">
                 </div>
 
 
@@ -58,37 +56,12 @@
             <span class="text-muted">Attachments</span>
             <!-- <span class="badge badge-secondary badge-pill">0</span> -->
           </h4>
-          <ul class="list-group mb-3">
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Product name</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$12</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Second product</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Third item</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$5</span>
-            </li>
+          <ul class="list-group mb-3" id="attachments-list">
+
           </ul>
 
           <form class="card p-2">
-            <div class="input-group">
-              <input type="text" class="form-control" placeholder="Promo code">
-              <div class="input-group-append">
-                <button type="submit" class="btn btn-secondary">Redeem</button>
-              </div>
-            </div>
+            <div id="drag-drop-area"></div>
           </form>
         </div>
     </div>
@@ -139,104 +112,188 @@
 </style>
 
 <script>
-    var data = [
-        { "Description": "Coca-Cola", "Price": 1.39},
-        { "Description": "Pringles", "Price": 4.73},
-        { "Description": "Meal Deal", "Price": 3.00},
-        { "Description": "Bananas", "Price": 2.03},
-        { "Description": "Batteries", "Price": 17.03},
-    ]
+    var claimFromServerRender = jQuery.parseJSON(<?php echo json_encode($claimJSON); ?>)
 
-    function insert_on_enter(field) {
-      field.on("keydown", function(e) {
-        if(e.keyCode === 13) {
-          $("#jsGrid").jsGrid("insertItem")
-          $("#jsGrid").jsGrid("clearInsert")
-          return false
-        }
-      })
-    }
+    $(document).ready(function() {
+        // Gets run once on page load (at bootom), then used to reload server data
+        function loadClaim(claim) {
+            claim.expenditure_items = jQuery.parseJSON(claim.expenditure_items)
+            $("#jsGrid").jsGrid('option', 'data', claim.expenditure_items)
 
-    function update_on_enter(field) {
-      field.on("keydown", function(e) {
-        if(e.keyCode === 13) {
-          $("#jsGrid").jsGrid("updateItem")
-          return false
-        }
-      })
-    }
-
-    function calculate_sum(args) {
-        $('#currency-sum').text(args.grid.data.reduce((accumulator, row) => accumulator + Number(row.Price), 0).toFixed(2))
-    }
- 
-    $("#jsGrid").jsGrid({
-        width: "100%",
- 
-        inserting: true,
-        editing: true,
-        sorting: false,
-        paging: false,
- 
-        data: data,
- 
-        fields: [
-            {
-                name: "Description",
-                type: "text",
-                width: 145,
-                validate: "required",
-                insertTemplate: function(value, item) {
-                    this.insertControl = $('<input type="text">').val(value)
-                    insert_on_enter(this.insertControl)
-                    return this.insertControl
-                },
-                editTemplate: function(value, item) {
-                    this.editControl = $('<input type="text">').val(value)
-                    update_on_enter(this.editControl)
-                    return this.editControl
-                },
-            },
-            {
-                name: "Price",
-                type: "number",
-                width: 35,
-                validate: "required",
-                insertTemplate: function(value, item) {
-                    this.insertControl = $('<input type="number">').attr('min', 0).attr('step', 0.01).val(value)
-                    insert_on_enter(this.insertControl)
-                    return this.insertControl
-                },
-                editTemplate: function(value, item) {
-                    this.editControl = $('<input type="number">').attr('min', 0).attr('step', 0.01).val(value)
-                    update_on_enter(this.editControl)
-                    return this.editControl
-                },
-                cellRenderer: function(value, item) {
-                    return $('<td>').addClass('currency').text(value)
-                },
-                insertValue: function() {
-                    return Number(this.insertControl.val())
-                },
-                editValue: function() {
-                    return Number(this.editControl.val())
-                },
-            },
-            {
-                type: "control",
-                width: 20
+        // Attachments
+            // Clear existing
+            $("#attachments-list").empty()
+            
+            if (claim.attachments.length === 0) {
+                $("<li>")
+                .addClass("list-group-item d-flex justify-content-between bg-light")
+                .append(
+                    $("<h6>")
+                    .addClass("my-0 text-muted")
+                    .text("No attachments added")
+                    )
+                .appendTo("#attachments-list")
             }
-        ],
-        onInit: calculate_sum,
-        onItemDeleted: calculate_sum,
-        onItemInserted: calculate_sum,
-        onItemUpdated: calculate_sum,
-    })
 
-    $('body').click(function(e) {
-        if ($(e.target).closest('#jsGrid').length === 0) {
-            $('#jsGrid').jsGrid('cancelEdit')
+            claim.attachments.forEach((attachment) => {
+                $("<li>")
+                .addClass("list-group-item d-flex justify-content-between lh-condensed")
+                .append(
+                    $("<div>")
+                    .append(
+                        $("<a>")
+                        .attr("href", "../../../uploads/"+attachment.id_filename)
+                        .append(
+                            $("<h6>")
+                            .addClass("my-0")
+                            .text(attachment.client_name)
+                            )
+                        )
+                    .append(
+                        $("<small>")
+                        .addClass("text-muted")
+                        .text(attachment.filesize_human)
+                        )
+                    )
+                // COMMENTED UNTIL DELETE FEATURE ADDED
+                // .append(
+                //     $("<span>")
+                //     .addClass("text-muted")
+                //     .text("X")
+                //     )
+                .appendTo("#attachments-list")
+            })
+
+        // Fields
+        $("#input_id_claim").val(claim.id_claim)
+        $("#input_claimant_id").val(claim.claimant_id)
+        $("#input_claimant_name").val(claim.claimant_name)
+        $("#input_date").val(claim.date)
+            $("#input_of_id_cost_centre").val(claim.of_id_cost_centre)
+            $("#input_description").val(claim.description)
+
         }
+
+
+        // jsGrid config
+        function insert_on_enter(field) {
+          field.on("keydown", function(e) {
+            if(e.keyCode === 13) {
+              $("#jsGrid").jsGrid("insertItem")
+              $("#jsGrid").jsGrid("clearInsert")
+              return false
+            }
+          })
+        }
+
+        function update_on_enter(field) {
+          field.on("keydown", function(e) {
+            if(e.keyCode === 13) {
+              $("#jsGrid").jsGrid("updateItem")
+              return false
+            }
+          })
+        }
+
+        function calculate_sum(args) {
+            $('#currency-sum').text(args.grid.data.reduce((accumulator, row) => accumulator + Number(row.Price), 0).toFixed(2))
+        }
+     
+        $("#jsGrid").jsGrid({
+            width: "100%",
+     
+            inserting: true,
+            editing: true,
+            sorting: false,
+            paging: false,
+     
+            data: [],
+     
+            fields: [
+                {
+                    name: "Description",
+                    type: "text",
+                    width: 145,
+                    validate: "required",
+                    insertTemplate: function(value, item) {
+                        this.insertControl = $('<input type="text">').val(value)
+                        insert_on_enter(this.insertControl)
+                        return this.insertControl
+                    },
+                    editTemplate: function(value, item) {
+                        this.editControl = $('<input type="text">').val(value)
+                        update_on_enter(this.editControl)
+                        return this.editControl
+                    },
+                },
+                {
+                    name: "Price",
+                    type: "number",
+                    width: 35,
+                    validate: "required",
+                    insertTemplate: function(value, item) {
+                        this.insertControl = $('<input type="number">').attr('min', 0).attr('step', 0.01).val(value)
+                        insert_on_enter(this.insertControl)
+                        return this.insertControl
+                    },
+                    editTemplate: function(value, item) {
+                        this.editControl = $('<input type="number">').attr('min', 0).attr('step', 0.01).val(value)
+                        update_on_enter(this.editControl)
+                        return this.editControl
+                    },
+                    cellRenderer: function(value, item) {
+                        return $('<td>').addClass('currency').text(value)
+                    },
+                    insertValue: function() {
+                        return Number(this.insertControl.val())
+                    },
+                    editValue: function() {
+                        return Number(this.editControl.val())
+                    },
+                },
+                {
+                    type: "control",
+                    width: 20
+                }
+            ],
+            onInit: calculate_sum,
+            onItemDeleted: calculate_sum,
+            onItemInserted: calculate_sum,
+            onItemUpdated: calculate_sum,
+            onOptionChanged: calculate_sum,
+        })
+
+        $('body').click(function(e) {
+            if ($(e.target).closest('#jsGrid').length === 0) {
+                $('#jsGrid').jsGrid('cancelEdit')
+            }
+        })
+
+
+        var uppy = Uppy.Core({
+            autoProceed: true,
+            debug: true,
+            restrictions: {
+                allowedFileTypes: ['image/gif', 'image/png', 'image/jpeg', 'image/jpg', 'application/pdf']
+            },
+
+        })
+        uppy.on('file-added', (file) => {
+            uppy.setFileMeta(file.id, {
+                invoice_id: <?php echo $claim['id_claim'] ?>
+            })
+        })
+        uppy.use(Uppy.DragDrop, { target: '#drag-drop-area' })
+        uppy.use(Uppy.XHRUpload, {
+            endpoint: '<?php echo site_url('/file/upload'); ?>',
+            fieldName: 'userfile',
+            getResponseError(responseText, xhr) {
+                alert('File failed to upload')
+                return new Error(JSON.parse(responseText).message)
+            },
+        })
+
+        loadClaim(claimFromServerRender)
     })
 
 </script>
