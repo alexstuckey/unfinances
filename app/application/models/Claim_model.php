1567645b28f7afb70185ab768382a57e8a7e1ea6
Alex Stuckey <alex@stuckey.org.uk>
2018-07-04 02:32:19 +0100

    Model for claims

---
+++ app/application/models/Claim_model.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Claim_model extends CI_Model {
+
+
+    //Loads the database using the ../config/database.php file
+    public function __construct()	{
+        $this->load->database();
+    }
+
+    public function getAllClaims()
+    {
+        $query = $this->db->get('claims');
+
+        return $query->result_array();
+    }
+
+    public function getClaimByID($id_claim)
+    {
+        $this->db->where('id_claim', $id_claim);
+        $query = $this->db->get('claims');
+
+        $claim = $query->row_array();
+
+
+        $this->load->model('File_model');
+        $claim['attachments'] = $this->File_model->getFilesForClaimID($id_claim);
+
+        return $claim;
+    }
+
+    public function createClaim($claimant_id)
+    {
+        $data = array(
+            'claimant_id' => $claimant_id,
+            'date' => date("Y-m-d"),
+            'expenditure_items' => "[]"
+        );
+
+        $this->db->insert('claims', $data);
+
+        return $this->db->insert_id();
+    }
+
+}
