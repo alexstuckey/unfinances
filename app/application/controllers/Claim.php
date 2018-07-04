e7917913cb18d1c20f00b2238cbb7316917cbad7
Alex Stuckey <alex@stuckey.org.uk>
2018-07-04 02:31:32 +0100

    New claim starts a new one, and then loads it

--- app/application/controllers/Claim.php
+++ app/application/controllers/Claim.php
@@ -13,8 +13,26 @@ class Claim extends CI_Controller {
         //     $data['is_admin'] = TRUE;
         // }
 
+        $this->load->model('Claim_model');
+        $id_claim = $this->Claim_model->createClaim($_SERVER['REMOTE_USER']);
+
+        $this->load->helper('url');
+        redirect('/expenses/claim/' . $id_claim);
+    }
+
+    public function showClaim($id_claim)
+    {
+        // $this->load->model('User_model');
+        // if (!$this->User_model->doesUserExist($_SERVER['REMOTE_USER'])) {
+        //     $this->load->helper('url');
+        //     redirect('/onboard/welcome');
+        // } else if ($this->User_model->isAdmin($_SERVER['REMOTE_USER'])) {
+        //     $data['is_admin'] = TRUE;
+        // }
+
         $data['active'] = 'expenses';
-        $data['page_title'] = 'UCFinances - New claim';
+        $data['page_title'] = 'UCFinances - New claim';$this->load->model('Claim_model');
+        $data['claim'] = $this->Claim_model->getClaimById($id_claim);
         
         $this->load->view('header', $data);
 
