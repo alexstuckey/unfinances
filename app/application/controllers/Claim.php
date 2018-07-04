4534ffb36bb93f3f5af239982e57cdb66277af7d
Alex Stuckey <alex@stuckey.org.uk>
2018-07-04 19:04:08 +0100

    JSON-ifying claim data + JSON method

--- app/application/controllers/Claim.php
+++ app/application/controllers/Claim.php
@@ -33,12 +33,32 @@ class Claim extends CI_Controller {
         $data['active'] = 'expenses';
         $data['page_title'] = 'UCFinances - New claim';$this->load->model('Claim_model');
         $data['claim'] = $this->Claim_model->getClaimById($id_claim);
+        // WORKAROUND: https://stackoverflow.com/a/6745634/298051
+        // Due to embedding JSON in JSON, the slashes are double escaped.
+        // Would use JSON_UNESCAPED_SLASHES option, but not available in targetted PHP 5.3
+        // So instead a find and replace is needed.
+        $data['claimJSON'] = json_encode($data['claim']);
+        $data['claimJSON'] = str_replace('\\\\\\', '\\', $data['claimJSON']);
         
         $this->load->view('header', $data);
 
         $this->load->view('claim_new', $data);
 
         $this->load->view('footer', $data);
+    }
+
+    public function jsonGetClaim($id_claim)
+    {
+        // auth
+
+        $data['claim'] = $this->Claim_model->getClaimById($id_claim);
+        $data['claimJSON'] = json_encode($data['claim']);
+        $data['claimJSON'] = str_replace('\\\\\\', '\\', $data['claimJSON']);
+
+        $this->output
+                ->set_status_header(201)
+                ->set_content_type('application/json')
+                ->set_output($data['claimJSON']);
     }
 
 }
