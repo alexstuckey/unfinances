cd9f2b7c004ce4ef09b79024c1920eff5cbf762a
Alex Stuckey <alex@stuckey.org.uk>
2018-07-11 23:06:56 +0100

    Controller for settings

--- app/application/controllers/Admin.php
+++ app/application/controllers/Admin.php
@@ -66,4 +66,56 @@ class Admin extends CI_Controller
         }
     }
 
+    public function settings()
+    {
+        $this->load->model('User_model');
+        // if (!$this->User_model->doesUserExist($_SERVER['REMOTE_USER'])) {
+        //     $this->load->helper('url');
+        //     redirect('/onboard/welcome');
+        // } else if ($this->User_model->isAdmin($_SERVER['REMOTE_USER'])) {
+        //     $data['is_admin'] = TRUE;
+        // } else {
+        //     $this->load->helper('url');
+        //     redirect('/home');
+        // }
+
+        $data['active'] = 'admin';
+        $data['active_admin'] = 'settings';
+        $data['page_title'] = 'Admin: Settings';
+
+        $data['admins'] = $this->User_model->getAdmins();
+
+        $this->load->library('form_validation');
+        $this->load->library('session');
+        $data['message'] = $this->session->flashdata('message');
+
+        $this->load->view('header', $data);
+
+        $this->load->view('admin_sidebar', $data);
+        $this->load->view('admin_page_settings', $data);
+        $this->load->view('admin_sidebar_close', $data);
+
+        $this->load->view('footer', $data);
+    }
+
+    public function settingsAddAdmin()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('adminUsernameAdd', 'Admin username', 'trim|required');
+        $this->form_validation->set_error_delimiters('<p class="alert alert-danger"><strong>Error: </strong>', '</p>');
+        $this->load->library('session');
+        if ($this->form_validation->run() == FALSE) {
+            $this->session->set_flashdata('error', validation_errors());
+            $this->settings();
+        } else {
+            $this->load->model('User_model');
+            if ($this->User_model->addAdmin($this->input->post('adminUsernameAdd'))) {
+                $this->session->set_flashdata('message', 'Admin added!');
+            } else {
+                $this->session->set_flashdata('error', 'Admin adding failed!');
+            }
+            $this->settings();
+        }
+    }
+
 }
