b555d891c9e26c9991daa0e31b0216f4664a2177
Alex Stuckey <alex@stuckey.org.uk>
2018-07-15 14:58:40 +0100

    Permission checking

--- app/application/controllers/Claim.php
+++ app/application/controllers/Claim.php
@@ -5,13 +5,11 @@ class Claim extends CI_Controller {
 
     public function newClaim()
     {
-        // $this->load->model('User_model');
-        // if (!$this->User_model->doesUserExist($_SERVER['REMOTE_USER'])) {
-        //     $this->load->helper('url');
-        //     redirect('/onboard/welcome');
-        // } else if ($this->User_model->isAdmin($_SERVER['REMOTE_USER'])) {
-        //     $data['is_admin'] = TRUE;
-        // }
+        $userAccount = $this->User_model->getUserByCIS($_SERVER['REMOTE_USER']);
+        if ($userAccount['doesUserExist'] == false) {
+            $this->load->helper('url');
+            redirect('/onboard/welcome');
+        }
 
         $this->load->model('Claim_model');
         $id_claim = $this->Claim_model->createClaim($_SERVER['REMOTE_USER']);
