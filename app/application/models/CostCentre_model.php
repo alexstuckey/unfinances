9abe085910e6f336a911deac51661dcfb3af158d
Alex Stuckey <alex@stuckey.org.uk>
2018-07-17 18:55:43 +0100

    Method to create cost centre

--- app/application/models/CostCentre_model.php
+++ app/application/models/CostCentre_model.php
@@ -5,7 +5,8 @@ class CostCentre_model extends CI_Model {
 
 
     //Loads the database using the ../config/database.php file
-    public function __construct()	{
+    public function __construct()
+    {
         $this->load->database();
     }
 
@@ -14,6 +15,20 @@ class CostCentre_model extends CI_Model {
         $query = $this->db->get('cost_centres');
 
         return $query->result_array();
+    }
+
+    public function createNewCostCentre($name)
+    {
+        if (!empty($name)) {
+            $data = array(
+                'cost_centre' => $name
+            );
+            $this->db->insert('cost_centres', $data);
+
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
