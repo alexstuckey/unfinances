1933ee89c5b1ad4c264e769894dc7be94f850c82
Alex Stuckey <alex@stuckey.org.uk>
2018-07-22 18:20:27 +0100

    Permission checking for comments

--- app/application/models/Activity_model.php
+++ app/application/models/Activity_model.php
@@ -65,15 +65,56 @@ class Activity_model extends CI_Model {
 
     public function commentOnClaimID($of_id_claim, $by_id_cis, $body)
     {
-        $data = array(
+
+        // Get claim
+        $this->load->model('Claim_model');
+        $claim = $this->Claim_model->getClaimByID($of_id_claim);
+
+        if (empty($claim)) {
+            $response['success'] = false;
+            $response['message'] = 'This claim does not exist.';
+        } else {
+
+            // Prep for permissions check
+            // Get user
+            $user = $this->User_model->getUserByCIS($by_id_cis);
+
+            $isManagerOfCostCentre = false;
+            foreach ($user['managerOfCostCentres'] as $cost_centre) {
+                if (isset($cost_centre['cost_centre']) && $cost_centre['cost_centre'] == $claim['cost_centre']) {
+                    $isManagerOfCostCentre = true;
+                }
+            }
+
+            // Check permissions
+            // 1. if it's your claim
+            // 2. if you are an admin
+            // 3. if you are a treasurer
+            // 4. if you are the manager of the cost centre of that claim
+            if ($claim['claimant_id'] == $user['username']) {
+                $response['success'] = true;
+            } else if ($user['is_admin'] || $user['is_treasurer'] || $isManagerOfCostCentre) {                
+                $response['success'] = true;
+            } else {
+                $response['success'] = false;
+                $response['message'] = 'You are not the owner of this claim.';
+            }
+        }
+
+        
+
+        // Update claim
+        if ($response['success']) {
+            $data = array(
             'of_id_claim' => $of_id_claim,
             'by_id_cis' => $by_id_cis,
             'activity_type' => 'comment',
             'activity_value' => $body
-            
-        );
+            );
+            $response['success'] = $this->db->insert('activities', $data);
+        }
 
-        return $this->db->insert('activities', $data);
+        return $response;
     }
 
     public function changeStatusOnClaimID($of_id_claim, $by_id_cis, $status_from, $status_to)
