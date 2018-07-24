6047777284540458f28d28d05870883a9ea12b35
Alex Stuckey <alex@stuckey.org.uk>
2018-07-24 12:43:09 +0100

    Client side for submission

--- app/application/views/claim_new.php
+++ app/application/views/claim_new.php
@@ -88,7 +88,7 @@
             <hr class="mb-5">
 
             <?php if ($claim['isEditable']): ?>
-            <div class="row form-group">
+            <div class="row form-group" id="save_claim_button_row">
                 <div class="col-6">
                     <button id="button_save" class="btn btn-dark btn-lg btn-block" role="button">Save</button>
                 </div>
@@ -407,9 +407,36 @@
     }
 
     // to progress the claim, by submitting it to Treasurer
-    window.claimStateToServer = function() {
+    window.submitClaimToServer = function() {
         if ($('#declaration-checkbox').is(':checked')) {
-            console.log("claim state")
+
+            // Prevent claiming when something has changed
+            if (window.stateChanged) {
+                alert('You have changed the claim form without saving. Please save or reload the page.')
+            } else {
+                jQuery.ajax({
+                    url: "../../api/expenses/submitClaim/"+window.claimState.id_claim,
+                    type: "POST",
+                    data: {}
+                }).done((data) => {
+                    console.log('Successfully claimed.', data)
+                    $("#save_claim_button_row").remove()
+                    refreshClaimStateFromServer()
+                }).fail((jqXHR, textStatus, errorThrown) => {
+                    if (jqXHR.status == 403) {
+                        console.error('Claim not editable, or you are not the owner.')
+                        alert('Claim not editable, or you are not the owner.')
+                    } else if (jqXHR.status == 404) {
+                        console.error('This claim does not exist')
+                        alert('Claim failed, this claim does not exist.')
+                    } else {
+                        console.error(errorThrown)
+                        alert('Save failed, please check your connection and try again.')
+                    }
+                    
+                })
+            }
+
         } else {
             alert('You must confirm the declaration before claiming an expense.')
         }
@@ -448,7 +475,7 @@
 
     // Link clicks
     $("#button_save").on("click", window.saveStateToServer)
-    $("#button_claim").on("click", window.claimStateToServer)
+    $("#button_claim").on("click", window.submitClaimToServer)
     // Detect when fields are altered
     $(".detectStateInput").change(window.checkStateChange)
     // Comment button / enter
