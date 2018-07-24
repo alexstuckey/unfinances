e16b46faa84e880ea9ca97e5ab23a1b04ffab97f
Alex Stuckey <alex@stuckey.org.uk>
2018-07-24 14:45:11 +0100

    Method to show claims for review

--- app/application/controllers/Expenses.php
+++ app/application/controllers/Expenses.php
@@ -26,4 +26,28 @@ class Expenses extends CI_Controller {
         $this->load->view('footer', $data);
     }
 
+    public function review()
+    {
+        $data['userAccount'] = $this->User_model->getUserByCIS($_SERVER['REMOTE_USER']);
+        if (!$data['userAccount']['is_treasurer']
+         && !$data['userAccount']['is_CostCentreManager']) {
+            show_error('You are not permitted to access this page.', 403, '403 Forbidden');
+        }
+
+        $data['active'] = 'expenses_review';
+        $data['subtitle'] = 'Expenses Review';
+        $data['page_title'] = 'UCFinances - ' . $data['subtitle'];
+        $data['page_lead_text'] = 'View all expense claims waiting for your review.';
+        $data['page_show_claimant_column'] = true;
+
+        $this->load->model('Claim_model');
+        $data['claims'] = $this->Claim_model->getClaimsForReviewByUser($data['userAccount']['username']);
+
+        $this->load->view('header', $data);
+
+        $this->load->view('expenses_table', $data);
+
+        $this->load->view('footer', $data);
+    }
+
 }
