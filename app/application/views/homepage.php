84b377baed55957708beac5f206cf300e1f3f6d1
Alex Stuckey <alex@stuckey.org.uk>
2018-07-30 15:39:05 +0100

    Homepage buttons to help on mobile

--- app/application/views/homepage.php
+++ app/application/views/homepage.php
@@ -1,18 +1,34 @@
 <!-- Content Block (Contains Sidebars and central columns) -->
 <main class="container-fluid" id="content">
-    <div class="jumbotron">
+    <div class="jumbotron" id="welcome-jumo">
         <h1>Welcome to UC Finances</h1>
         <p class="lead">
-            Currently unfunctional.
+            Follow the buttons below to file a claim or review your expenses.
         </p>
+    </div>
+    <div class="jumbotron">
+        <div class="row justify-content-center">
+            <div class="col-md-4 pb-3 pb-md-0">
+                <a href="<?php echo site_url('/expenses/claim/new'); ?>" class="btn btn-primary btn-block" role="button">New Claim</a>
+            </div>
+            <div class="col-md-4 pb-3 pb-md-0">
+                <a href="<?php echo site_url('/expenses/my'); ?>" class="btn btn-primary btn-block" role="button">My Expenses</a>
+            </div>
+            <?php if ((!empty($userAccount['is_treasurer']) && ($userAccount['is_treasurer'] == TRUE))
+                   || (!empty($userAccount['is_CostCentreManager']) && ($userAccount['is_CostCentreManager'] == TRUE))): ?>
+            <div class="col-md-4">
+                <a href="<?php echo site_url('/expenses/review'); ?>" class="btn btn-primary btn-block" role="button">Expenses Review</a>
+            </div>
+            <?php endif; ?>
+        </div>
     </div>
 </main>
 
 <style type="text/css">
-   .jumbotron {
+   #welcome-jumo {
         position: relative;
     }
-    .jumbotron:after {
+    #welcome-jumo:after {
         content: "";
         top: 0;
         left: 0;
@@ -28,7 +44,7 @@
         background-size: auto 85%;
         opacity: 0.5;
     }
-    .jumbotron h1 {
+    #welcome-jumo h1 {
         z-index: 2;
     }
 </style>
