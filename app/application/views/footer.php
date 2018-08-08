b93bb35b6bfd7130fd7644d924ab720f45ed36d4
Alex Stuckey <alex@stuckey.org.uk>
2018-08-08 23:53:10 +0100

    Improved footer

--- app/application/views/footer.php
+++ app/application/views/footer.php
@@ -1,5 +1,5 @@
             <footer class="my-5 text-muted text-center text-small">
-                <p class="mb-0">© 2017-2018 Alex Stuckey</p>
+                <p class="mb-0">Made by Alex Stuckey</p>
             </footer>
         </div>
     </body>
